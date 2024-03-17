2828d474b8ed73f4cc59901f0aff17320b4ae0be
tah-ir-7878 <145276875+tah-ir-7878@users.noreply.github.com>
2024-03-18 00:25:31 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    { message :"Enter first number", type :"number", name: "firstnumber"},
+     {message: "Enter Second number", type:"number", name:"secondnumber"},
+{ message: "Select one of the operators to perform operation", type:"list", name: "operation",
+choices:["Addition","Subtraction","Multiplication","Division"]},
+])
+
+// conditional statement
+if (answer.operation === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operation === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operation === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operation === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+} else  {
+    console.log("Please select valid operator");
+}
+
+
+
+
+
+
+
+
+
+
+
